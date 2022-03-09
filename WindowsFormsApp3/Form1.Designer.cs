f7e3ffb08b906c7cc43a4d9a8bbea2806f7a236a
nguyendinhkha <82517228+nguyendinhkha@users.noreply.github.com>
2022-03-09 09:44:25 +0700

    Lab1_Fixed

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.num1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.num2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.num3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.largest = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.smallest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Số thứ nhất";
+            // 
+            // num1
+            // 
+            this.num1.Location = new System.Drawing.Point(131, 62);
+            this.num1.Name = "num1";
+            this.num1.Size = new System.Drawing.Size(108, 22);
+            this.num1.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(37, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Số thứ hai";
+            // 
+            // num2
+            // 
+            this.num2.Location = new System.Drawing.Point(131, 121);
+            this.num2.Name = "num2";
+            this.num2.Size = new System.Drawing.Size(108, 22);
+            this.num2.TabIndex = 16;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(37, 184);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 16);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Số thứ ba";
+            // 
+            // num3
+            // 
+            this.num3.Location = new System.Drawing.Point(131, 178);
+            this.num3.Name = "num3";
+            this.num3.Size = new System.Drawing.Size(108, 22);
+            this.num3.TabIndex = 18;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(341, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Tìm";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(341, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Xoá";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(341, 181);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Số lớn nhất";
+            // 
+            // largest
+            // 
+            this.largest.Location = new System.Drawing.Point(531, 94);
+            this.largest.Name = "largest";
+            this.largest.ReadOnly = true;
+            this.largest.Size = new System.Drawing.Size(99, 22);
+            this.largest.TabIndex = 23;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(528, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 16);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "Số nhỏ nhất";
+            // 
+            // smallest
+            // 
+            this.smallest.Location = new System.Drawing.Point(531, 177);
+            this.smallest.Name = "smallest";
+            this.smallest.ReadOnly = true;
+            this.smallest.Size = new System.Drawing.Size(100, 22);
+            this.smallest.TabIndex = 25;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 458);
+            this.Controls.Add(this.smallest);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.largest);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.num3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.num2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.num1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox num1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox num2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox num3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox largest;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox smallest;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a1 = float.Parse(num1.Text.Trim());
+            float a2 = float.Parse(num2.Text.Trim());
+            float a3 = float.Parse(num3.Text.Trim());
+            if (a1 >= a2)
+            {
+                if (a2 >= a3)
+                {
+                    largest.Text = a1.ToString();
+                    smallest.Text = a3.ToString();
+                }
+                else
+                {
+                    if (a1 >= a3)
+                    {
+                        largest.Text = a1.ToString();
+                    }
+                    else
+                    {
+                        largest.Text = a3.ToString();
+                    }
+                    smallest.Text = a2.ToString();
+                }
+            }
+            else
+            {
+                if (a1 >= a3)
+                {
+                    largest.Text = a2.ToString();
+                    smallest.Text = a3.ToString();
+                }
+                else
+                {
+                    if (a2 >= a3)
+                    {
+                        largest.Text = a2.ToString();
+                    }
+                    else
+                    {
+                        largest.Text = a3.ToString();
+                    }
+                    smallest.Text = a1.ToString();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            num1.Text = num2.Text = num3.Text = largest.Text = smallest.Text = null;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
